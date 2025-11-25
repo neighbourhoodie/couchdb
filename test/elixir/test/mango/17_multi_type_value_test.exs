68ab391345442bdc93ed39a9c20ea731fd8ccaf2
Ninette <ninette.adhikari@gmail.com>
2025-11-25 17:07:53 +0100

    update test to use macro for MultiValueFieldTests

--- test/elixir/test/mango/17_multi_type_value_test.exs
+++ test/elixir/test/mango/17_multi_type_value_test.exs
@@ -12,15 +12,87 @@
 
 defmodule MultiValueFieldTests do
   use CouchTestCase
+  defmacro describe(db) do
+    quote do
+      test "can query with name" do
+        selector = %{"name" => %{"$exists" => true}}
+        {:ok, docs} = MangoDatabase.find(unquote(db), selector)
 
-  @db_name "multi-type-value-docs"
-  @docs [
-    %{"_id" => "1", "name" => "Jimi", "age" => 10},
-    %{"_id" => "2", "name" => %{"forename" => "Eddie"}, "age" => 20},
-    %{"_id" => "3", "name" => nil, "age" => 30},
-    %{"_id" => "4", "name" => 1, "age" => 40},
-    %{"_id" => "5", "forename" => "Sam", "age" => 50}
-  ]
+        assert length(docs) == 4
+        for doc <- docs do
+          assert Map.has_key?(doc, "name")
+        end
+      end
+
+      test "can query with name subfield" do
+        selector = %{"name.forename" => %{"$exists" => true}}
+        {:ok, docs} = MangoDatabase.find(unquote(db), selector)
+
+        assert length(docs) == 1
+        assert Enum.at(docs, 0)["_id"] == "2"
+      end
+
+      test "can query with name range" do
+        selector = %{"name" => %{"$gte" => 0}}
+        {:ok, docs} = MangoDatabase.find(unquote(db), selector)
+
+        assert length(docs) == 3
+        for doc <- docs do
+          assert Map.has_key?(doc, "name")
+          assert doc["name"] in [1, "Jimi", %{"forename" => "Eddie"}]
+        end
+      end
+
+      test "can query with age and name range" do
+        selector = %{"age" => %{"$gte" => 0, "$lt" => 40}, "name" => %{"$gte" => 0}}
+        {:ok, docs} = MangoDatabase.find(unquote(db), selector)
+
+        assert length(docs) == 2
+        for doc <- docs do
+          assert Map.has_key?(doc, "name")
+          assert doc["name"] in [1, "Jimi", %{"forename" => "Eddie"}]
+        end
+      end
+    end
+  end
+end
+
+defmodule MultiValueFieldTestData do
+  def docs do
+    [
+      %{"_id" => "1", "name" => "Jimi", "age" => 10},
+      %{"_id" => "2", "name" => %{"forename" => "Eddie"}, "age" => 20},
+      %{"_id" => "3", "name" => nil, "age" => 30},
+      %{"_id" => "4", "name" => 1, "age" => 40},
+      %{"_id" => "5", "forename" => "Sam", "age" => 50}
+    ]
+  end
+end
+
+defmodule MultiValueFieldJSONTests do
+  use CouchTestCase
+  require MultiValueFieldTests
+
+  @docs MultiValueFieldTestData.docs()
+  @db_name "multi-value-field-json"
+
+  setup do
+    MangoDatabase.recreate(@db_name)
+    MangoDatabase.save_docs(@db_name, @docs)
+    MangoDatabase.create_index(@db_name, ["name"])
+    MangoDatabase.create_index(@db_name, ["age", "name"])
+    :ok
+  end
+
+  MultiValueFieldTests.describe(@db_name)
+end
+
+defmodule MultiValueFieldAllDocsTests do
+  use CouchTestCase
+  require MultiValueFieldTests
+
+  @docs MultiValueFieldTestData.docs()
+  @db_name "multi-value-field-all-docs"
 
   setup do
     MangoDatabase.recreate(@db_name)
@@ -28,43 +100,5 @@ defmodule MultiValueFieldTests do
     :ok
   end
 
-  test "can query with name" do
-    selector = %{"name" => %{"$exists" => true}}
-    {:ok, docs} = MangoDatabase.find(@db_name, selector)
-
-    assert length(docs) == 4
-    for doc <- docs do
-      assert Map.has_key?(doc, "name")
-    end
-  end
-
-  test "can query with name subfield" do
-    selector = %{"name.forename" => %{"$exists" => true}}
-    {:ok, docs} = MangoDatabase.find(@db_name, selector)
-
-    assert length(docs) == 1
-    assert Enum.at(docs, 0)["_id"] == "2"
-  end
-
-  test "can query with name range" do
-    selector = %{"name" => %{"$gte" => 0}}
-    {:ok, docs} = MangoDatabase.find(@db_name, selector)
-
-    assert length(docs) == 3
-    for doc <- docs do
-      assert Map.has_key?(doc, "name")
-      assert doc["name"] in [1, "Jimi", %{"forename" => "Eddie"}]
-    end
-  end
-
-  test "can query with age and name range" do
-    selector = %{"age" => %{"$gte" => 0, "$lt" => 40}, "name" => %{"$gte" => 0}}
-    {:ok, docs} = MangoDatabase.find(@db_name, selector)
-
-    assert length(docs) == 2
-    for doc <- docs do
-      assert Map.has_key?(doc, "name")
-      assert doc["name"] in [1, "Jimi", %{"forename" => "Eddie"}]
-    end
-  end
+  MultiValueFieldTests.describe(@db_name)
 end
